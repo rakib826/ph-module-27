cbc04bb36395f6bf8f239a69b491d9a625c2e67d
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2024-09-19 16:30:27 +0600

    event handler added for phone number

---
+++ Payoo-MFS/js/login.js
@@ -0,0 +1,7 @@
+document.getElementById("button-login").addEventListener("click",function(event){
+  event.preventDefault();
+  console.log("login button clicked")
+
+  const phoneNumber = document.getElementById("phone-number");
+  console.log(phoneNumber)
+})
